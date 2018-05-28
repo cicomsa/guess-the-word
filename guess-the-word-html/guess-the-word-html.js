a0bccd76c82d69653c416d04d2d00da8c5979585
cicomsa <ioanacomsa85@gmail.com>
2018-05-28 18:28:27 +0200

    organised code for visibility

--- guess-the-word-html/guess-the-word-html.js
+++ guess-the-word-html/guess-the-word-html.js
@@ -6,9 +6,10 @@ let letterArray = []
 function showGuess(word, guesses) {
   guessArray = []
   splitWord = word.split("")
-  splitWord.filter(n => guesses.indexOf(n) != -1 ? guessArray.push(n) : guessArray.push("_"))
+  splitWord.filter(n => guesses.indexOf(n) != -1 ?
+      guessArray.push(n) : guessArray.push("_"))
   guessArray = guessArray.join(" ")
-  return guessArray
+    return guessArray
 }
 
 function play(word, guesses) {
@@ -33,12 +34,14 @@ function play(word, guesses) {
     }
   }
 
-  answer = prompt("Word to guess: " + dashes + "\nInput letter or write 'exit' to end the game:")
+  answer = prompt("Word to guess: " + dashes 
+    + "\nInput letter or write 'exit' to end the game:")
   if (answer === 'exit' || answer === null) {
     alert("Game over")
   } else {
     game()
-    while (guessed.indexOf("_") >= 0 && tries < 6 && answer !== "exit"  && answer !== null) {
+    while (guessed.indexOf("_") >= 0 && tries < 6 
+      && answer !== "exit"  && answer !== null) {
       answer = prompt("Word to guess: " + guessed + "\nInput letters: " + letterArray + "\nInput letter:")
       game()
     }
